2db538bdfe8df0fc96f43484fcb785c0f6d79d60
Tylermhh <tyler.hlaing@gmail.com>
2025-03-23 13:09:29 -0700

    added instructions for error of ImportError: cannot import name 'model_from_config' from 'tensorflow.keras.models'

--- practice/RL Practice.py
+++ practice/RL Practice.py
@@ -1,9 +1,31 @@
 # pip install gym==0.25.2 tensorflow keras-rl2 pygame==2.6.0 numpy==1.26.0
+# go to environment's Lib/site-packages/rl/util.py and replace model_from_config with model_from_json 
 
 import random
 import gym
 
+# import tensorflow as tf
+# print(tf.__version__)
+# from tensorflow.keras.models import Sequential
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
 env = gym.make("CartPole-v1", render_mode="human")
+
+states = env.observation_space.shape[0]
+actions = env.action_space.n
+
+# print(actions)
+# print(states)
+
+
 
 episodes = 10
 
