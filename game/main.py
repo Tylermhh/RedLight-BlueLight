9849f9b56b4e988e088d7fd89b29642d279ebfbe
briancsp <runnerb2003@gmail.com>
2025-04-13 13:59:37 -0700

    change red/green light states after certain time interval

--- game/main.py
+++ game/main.py
@@ -1,17 +1,15 @@
 from constants import *
 from random import random
-
-
-def get_green_light_time_seconds() -> float:
-    return random() * GREEN_LIGHT_TIME_RANGE_SECONDS + GREEN_LIGHT_TIME_MIN_SECONDS
+from datetime import datetime, timedelta
 
 
 class Game:
     def __init__(self):
         self.state: GameState = GameState.READ_FACES
-        self.face_filters_playing: list = []
-        self.face_filters_won: list = []
-        self.face_filters_lost: list = []
+        self.face_filters_playing: list = [] # list of what?
+        self.face_filters_won: list = [] # list of what?
+        self.face_filters_lost: list = [] # list of what?
+        self.time_for_next_state = datetime.now()
 
     def run(self):
         # game loop skeleton
@@ -32,35 +30,56 @@ class Game:
         pass
 
         # start red light / green light loop
-        self.state = GameState.GREEN_LIGHT
+        self.to_green_light_state()
 
     def green_light(self):
         # stay green for a random amount of time
         # time is predetermined when green light state is first entered
-        pass
+        if self.time_for_next_state < datetime.now() and self.face_filters_playing:
+            # if there are still players left playing, go to red light state
+            self.to_red_light_state()
 
         # if no players left playing, end game
-        # otherwise, go to red light state
         if not self.face_filters_playing:
             self.state = GameState.END_GAME
-        else:
-            self.state = GameState.RED_LIGHT
 
     def red_light(self):
         # stay red for defined amount of time (RED_LIGHT_TIME_SECONDS)
         # check for movement during this time
-        pass
+        if self.time_for_next_state < datetime.now() and self.face_filters_playing:
+            # if there are still players left playing, go to green light state
+            self.to_green_light_state()
+        else:
+            # check for movement each frame
+            pass
 
         # if no players left playing, end game
-        # otherwise, go to green light state
         if not self.face_filters_playing:
             self.state = GameState.END_GAME
-        else:
-            self.state = GameState.GREEN_LIGHT
 
     def end_game(self):
         # report results
         pass
+
+    def to_green_light_state(self):
+        self.set_time_for_next_state_green()
+        self.state = GameState.GREEN_LIGHT
+
+    def to_red_light_state(self):
+        self.set_time_for_next_state_red()
+        self.state = GameState.RED_LIGHT
+
+    def set_time_for_next_state_green(self):
+        green_light_time_seconds = random() * (
+                GREEN_LIGHT_TIME_RANGE_SECONDS + GREEN_LIGHT_TIME_MIN_SECONDS)
+        self.time_for_next_state = (
+                datetime.now() + timedelta(seconds=green_light_time_seconds)
+        )
+
+    def set_time_for_next_state_red(self):
+        self.time_for_next_state = (
+            datetime.now() + timedelta(seconds=RED_LIGHT_TIME_SECONDS)
+        )
 
 
 def main():
